f6ea95e66f271db64b96dabe26a2f4f42c7658be
Janne Hakonen <janne.hakonen@oss.tieto.com>
2013-11-21 11:53:13 +0200

    [plugin-runner] Send random key events for testing purposes.
    Signed-off-by: Janne Hakonen <janne.hakonen@oss.tieto.com>

--- tools/plugin-runner/main.cpp
+++ tools/plugin-runner/main.cpp
@@ -1,8 +1,24 @@
 #include <QtGui/QGuiApplication>
 #include <QQuickItem>
 #include <QTimer>
+#include <QKeyEvent>
 #include "qtquick2applicationviewer.h"
 #include "keysource.h"
+
+QEvent::Type getRandomKeyState() {
+    return (qrand() % 2)? QEvent::KeyPress: QEvent::KeyRelease;
+}
+
+int getRandomKey() {
+    switch (qrand() % 3) {
+    case 0:
+        return Qt::Key_VolumeDown;
+    case 1:
+        return Qt::Key_VolumeUp;
+    case 2:
+        return Qt::Key_PowerOff;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -34,6 +50,14 @@ int main(int argc, char *argv[])
         QMetaObject::invokeMethod(volumeUp, "flash");
         volumeUp->setProperty("isPressed", pressed);
     });
+
+    // fakey sending of events
+    QTimer fooTimer;
+    QObject::connect(&fooTimer, &QTimer::timeout, [&]() {
+        QGuiApplication::sendEvent(&app, new QKeyEvent(getRandomKeyState(), getRandomKey(), Qt::NoModifier));
+    });
+    fooTimer.setInterval(1000);
+    fooTimer.start();
 
     return app.exec();
 }
